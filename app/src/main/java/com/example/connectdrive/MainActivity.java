6b9ac3794b1e0d65d980fa82d56c58d180b40d10
GabfestFree <manikandan.12it@gmail.com>
2016-09-28 00:25:39 +0530

    Successfully Re added fragments

--- app/src/main/java/com/example/connectdrive/MainActivity.java
+++ app/src/main/java/com/example/connectdrive/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+//main Activity
     TextView login_button;
     TextView register_button;
     @Override
