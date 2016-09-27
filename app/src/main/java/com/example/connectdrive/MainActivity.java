496684323e8ad160117898a1536a326c7b6a4c31
GabfestFree <manikandan.12it@gmail.com>
2016-09-28 00:10:04 +0530

    Successfully added fragments

--- app/src/main/java/com/example/connectdrive/MainActivity.java
+++ app/src/main/java/com/example/connectdrive/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.connectdrive;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView login_button;
+    TextView register_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login_button= (TextView) findViewById(R.id.login_button);
+        register_button=(TextView)findViewById(R.id.register_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Navigation_Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
